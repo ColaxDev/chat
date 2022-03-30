89b437e72252bf039d26ea6bac33a202b2f26254
ColaxDev <colax.dev@gmail.com>
2022-03-30 16:18:57 +0200

    Add commands

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	s := newServer()
+	go s.run()
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("unable to start server: %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("started server on :8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("unable to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
 }
